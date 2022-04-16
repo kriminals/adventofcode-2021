b5d1398e16701f27a8b3cf8e9ed07063a8b0c2d4
kriminals <krimini.kriminals@gmail.com>
2022-04-16 20:22:07 +0300

    Added day 12 part 2

---
+++ day12_1.js
@@ -0,0 +1,84 @@
+let input = `xz-end
+CJ-pt
+pt-QW
+hn-SP
+pw-CJ
+SP-end
+hn-pt
+GK-nj
+fe-nj
+CJ-nj
+hn-ZZ
+hn-start
+hn-fe
+ZZ-fe
+SP-nj
+SP-xz
+ZZ-pt
+nj-ZZ
+start-ZZ
+hn-GK
+CJ-end
+start-fe
+CJ-xz`
+let vertices = new Set(input.split('\n').flatMap(entry => entry.split('-')))
+console.log(vertices)
+let edges = input.split('\n').map(entry => entry.split('-'))
+console.log(edges)
+function createAdjacentVertices(edges) {
+    // Initialise Empty Adjacent Vertices
+    let adjacentVertices = new Map()
+    for (let vertice of vertices) {
+        adjacentVertices.set(vertice, [])
+    }
+    edges.forEach(edge => {
+        adjacentVertices.get(edge[0]).push(edge[1])
+        adjacentVertices.get(edge[1]).push(edge[0])
+    });
+    return adjacentVertices
+}
+let adjacentVertices = createAdjacentVertices(edges)
+function findAdjacentVertices(vertix) {
+    return adjacentVertices.get(vertix)
+}
+
+function isCaveTwice(cave, path) {
+    return path.filter(element => element === cave).length === 2
+}
+
+function hasDoubleCave(path) {
+    let smallCavesPath = path.filter(cave => cave.toLowerCase() === cave || cave === 'start' || cave === 'end')
+    const pathSet = new Set(smallCavesPath)
+    return pathSet.size !== smallCavesPath.length
+     
+}
+
+let sollutions = 0
+function bfs(vertix, path) {
+    let queue = findAdjacentVertices(vertix)
+    path.push(vertix)
+    
+    queue.forEach(element => {
+        if (element === 'end') {
+            sollutions++
+            console.log(path)
+            return
+        }
+        // Check if element is small letters and is visited twice
+        if (isCaveTwice(element, path) && (element.toLowerCase() === element)) {
+            return 
+        }
+        // Check if element is small letter is already visited and path has already a double cave
+        if (path.includes(element) && (element.toLowerCase() === element) && hasDoubleCave(path)) {
+            return
+        }
+        // Check if i return to start
+        if (path.includes(element) && (element.toLowerCase() === 'start')) {
+            return 
+        }
+        let newPath = [...path]
+        bfs(element, newPath)
+    })
+}
+bfs('start', [])
+console.log(sollutions)
